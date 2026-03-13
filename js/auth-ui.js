82920b96cf2b94791836bb4eef5e949390551ebf
JulieKristoffersen <Julie.kristoffersen01@yahoo.com>
2026-03-13 12:13:09 +0100

    Added auth-ui file and updated nav
    Added auth-ui file for making the nav menu show the links based on wether the user are logged in or not. Also changed the nav in the index files to match this setup an actuallly respond to the js.

---
+++ js/auth-ui.js
@@ -0,0 +1,30 @@
+import { supabase } from "./supabase.js"
+
+const loginLink = document.getElementById("loginLink")
+const registerLink = document.getElementById("registerLink")
+const logoutLink = document.getElementById("logoutLink")
+const createLink = document.getElementById("createLink")
+
+async function checkAuth() {
+
+  const { data } = await supabase.auth.getUser()
+
+  if (data.user) {
+
+    loginLink.style.display = "none"
+    registerLink.style.display = "none"
+
+  } else {
+
+    logoutLink.style.display = "none"
+    createLink.style.display = "none"
+
+  }
+
+}
+checkAuth()
+
+logoutLink?.addEventListener("click", async () => {
+  await supabase.auth.signOut()
+  window.location.href = "index.html"
+})
