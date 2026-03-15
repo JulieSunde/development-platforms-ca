aaefde0ae24f7f651aed9baadea681a8da2ff813
JulieKristoffersen <Julie.kristoffersen01@yahoo.com>
2026-03-15 16:24:09 +0100

    Added the Supabase keys again
    Not sure if we should have the Supabase keys in this assignment, but without them the page is not working so I implented them again..

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = 
-const supabaseKey = 
+const supabaseUrl = "https://nffqqhmyotnwznuzhkfv.supabase.co";
+const supabaseKey = "sb_publishable_RiMDqj8R2qqORUsKvDHlww_6riOX6BK";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
